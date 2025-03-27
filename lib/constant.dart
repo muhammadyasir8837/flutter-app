01cbf7168bf879a0329a1a1f200492011f7aa799
The  Tech Developer <muhammadyasir8837@gmail.com>
2025-03-27 12:17:50 +0500

    calculator_app

---
+++ lib/constant.dart
@@ -0,0 +1,7 @@
+
+
+import 'package:flutter/material.dart';
+const Color whiteColor = Colors.white;
+const Color grayColor =  Colors.grey;
+const Color blueColor =  Colors.blue;
+const headingTextStyle =  TextStyle(fontSize: 30,color: blueColor,fontWeight: FontWeight.w500);
